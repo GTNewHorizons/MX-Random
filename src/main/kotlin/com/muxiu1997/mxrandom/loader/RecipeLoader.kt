a141bae18d0294c0515d4c7a5fea61ff72256c6d
Jakub <kuba.123123.6000@gmail.com>
2024-12-31 16:19:37 +0100

    Add LMA recipe back (#16)
    (Same as commit 4902a85e8002d12da381ad2d42eff7bb17518656 but in scala)

--- src/main/kotlin/com/muxiu1997/mxrandom/loader/RecipeLoader.kt
+++ src/main/kotlin/com/muxiu1997/mxrandom/loader/RecipeLoader.kt
@@ -1,7 +1,27 @@
 package com.muxiu1997.mxrandom.loader
 
 import cpw.mods.fml.common.event.FMLPostInitializationEvent
+import gregtech.api.enums.GTValues
+import gregtech.api.enums.ItemList
+import gregtech.api.enums.Materials
+import gregtech.api.enums.Mods.AppliedEnergistics2
+import gregtech.api.enums.TierEU
+import gregtech.api.recipe.RecipeMaps.assemblerRecipes
+import gregtech.api.util.GTModHandler.getModItem
+import gregtech.api.util.GTRecipeBuilder.SECONDS
 
 object RecipeLoader {
-  fun load(@Suppress("UNUSED_PARAMETER") e: FMLPostInitializationEvent) {}
+  fun load(@Suppress("UNUSED_PARAMETER") e: FMLPostInitializationEvent) {
+    GTValues.RA.stdBuilder()
+        .itemInputs(
+            getModItem(AppliedEnergistics2.ID, "tile.BlockInterface", 8, 0),
+            getModItem(AppliedEnergistics2.ID, "tile.BlockMolecularAssembler", 8, 0),
+            ItemList.Emitter_IV.get(4L),
+            ItemList.Casing_RobustTungstenSteel.get(1L))
+        .itemOutputs(GTMetaTileEntityLoader.largeMolecularAssembler.getStackForm(1))
+        .fluidInputs(Materials.Plastic.getMolten(1296))
+        .duration(60 * SECONDS)
+        .eut(TierEU.RECIPE_IV)
+        .addTo(assemblerRecipes)
+  }
 }
